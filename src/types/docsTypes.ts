40f5f830013a26bbfe4e12fa4b33807150ad3615
nousantx <nousantx@gmail.com>
2024-04-25 20:46:34 +0700

    fix(types): Added name types for RouteDocs

--- src/types/docsTypes.ts
+++ src/types/docsTypes.ts
@@ -1,10 +1,12 @@
 export interface RouteGroup {
+  name: string;
   slug: string;
   routes: Route[];
 }
 
 export interface Route {
   name: string;
+  desc: string;
   slug: string;
   element: React.ComponentType<any>;
 }
