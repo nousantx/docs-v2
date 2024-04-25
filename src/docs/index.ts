9dd64a7e1ce162098de5390b6992ce4f9ef82063
nousantx <nousantx@gmail.com>
2024-04-25 20:44:23 +0700

    feat(docs): Add naming for each category

--- src/docs/index.ts
+++ src/docs/index.ts
@@ -1,13 +1,14 @@
 import { Functions } from "./functions";
 import { Classes } from "./classes";
 import { RouteGroup } from "../types/docsTypes";
-
 export const Routes: RouteGroup[] = [
   {
+    name: "Classes Documentation",
     slug: "/classes",
     routes: Classes,
   },
   {
+    name: "Functions",
     slug: "/functions",
     routes: Functions,
   },
