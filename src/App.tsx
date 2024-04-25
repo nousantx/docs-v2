2fe081be73020c48c931c9684b8aa90b153bfe7b
nousantx <nousantx@gmail.com>
2024-04-25 10:34:12 +0700

    feat(app): Add simple initial page for docs-v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Documentation from "./pages/Documentation";
 import { useStyles } from "./utils/useStyles";
+// import OtherPage from "./OtherPage"; // Import other pages/components here
 
-function App() {
+const App: React.FC = () => {
   useStyles();
-  return <h1 className="tc-red">Hello</h1>;
-}
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <h1>This is homepage</h1>
+          </>
+        }
+      />
+      <Route path="/docs/*" element={<Documentation />} />
+
+      <Route
+        path="/other"
+        element={
+          <>
+            <h1>This is other page!</h1>
+          </>
+        }
+      />
+    </Routes>
+  );
+};
 
 export default App;
