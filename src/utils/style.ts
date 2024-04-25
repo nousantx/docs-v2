b5f8e8b13481f55dd35177472389bdd8ff0ba610
nousantx <nousantx@gmail.com>
2024-04-25 10:32:39 +0700

    feat(style): Add new props, styles, and selectors on style.ts

--- src/utils/style.ts
+++ src/utils/style.ts
@@ -2,8 +2,9 @@ type Styles = Record<string, string | Record<string, string>>;
 
 // all styles
 export const styles: Styles = {
-  selector: "styles",
-  nested: {
-    "": "styles for nested",
-  },
+  "a.breadcrumb-link": "tc-blue",
+  "a.breadcrumb-link.last.active": "tc-red",
+  html: "scroll-smooth",
+  body: "family-[tx_font-body]",
+  ".flex-center": "display-flex flex-parent-center",
 };
