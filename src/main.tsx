f2fc2b4d9b2e1110309f9bf9fad71e7f52e66c19
nousantx <nousantx@gmail.com>
2024-04-25 10:33:32 +0700

    fix(main): Adding BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
 import "./css/color.css";
 import "./css/root.css";
+import "./css/icons/google-icons.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
