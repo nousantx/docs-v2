f17270ce2898da10be3da31b42f5ba3b772d7939
nousantx <nousantx@gmail.com>
2024-04-26 15:13:12 +0700

    feat(docs): Add get started page

---
+++ src/docs/get-started.tsx
@@ -0,0 +1,31 @@
+import Code from "../components/Code";
+import { useStyles } from "../hooks/useStyles";
+
+const GetStarted = () => {
+  useStyles();
+  return (
+    <section className="p-2rem w-full">
+      <header>
+        <h1 className="text-4xl fw-700">Get Started</h1>
+        <p className="text-lg fw-300 mt-6px">
+          Let's learn about tenoxui css framework. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Ex, illum.
+        </p>
+      </header>
+      <h2 className="text-3xl fw-600 mt-2rem mb-1rem">
+        Installation{" "}
+        <a href="#installation" className="text-primary">
+          #
+        </a>
+      </h2>
+
+      <Code title="Installation" language="bash">
+        {`npm i tenoxui --save-dev
+#or
+yarn add tenoxui -D`}
+      </Code>
+    </section>
+  );
+};
+
+export default GetStarted;
