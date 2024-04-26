184b3d0b19e1a04b4c35d262f35b7ee113d40f89
nousantx <nousantx@gmail.com>
2024-04-26 15:10:00 +0700

    feat(pages): Testing layout new layout

--- src/pages/Documentation.tsx
+++ src/pages/Documentation.tsx
@@ -1,20 +1,23 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Breadcrumbs from "../components/Breadcrumbs";
-import DocsSidebar from "../components/DocsSidebar";
+// import DocsSidebar from "../components/DocsSidebar";
 import { Routes as DocsRoutes } from "../docs";
+import GetStarted from "../docs/get-started";
+import DocsSidebar from "../components/DocsSidebar";
 
 const Documentation: React.FC = () => {
   return (
     <>
-      <div style={{ display: "flex" }}>
-        {/* Sidebar */}
+      <div className="grid-container">
         <DocsSidebar />
-        {/* Content */}
-        <div>
-          {/* Breadcrumbs */}
-          <Breadcrumbs />
+
+        <div className="main-content">
+          <div className="ph-2rem">
+            <Breadcrumbs />
+          </div>
           <Routes>
             <Route path="/">
+              <Route path="get-started" element={<GetStarted />} />
               {/* Render routes for each category */}
               {DocsRoutes.map((routeGroup) => (
                 <Route
@@ -45,10 +48,13 @@ const Documentation: React.FC = () => {
                     path={`${routeGroup.slug}${route.slug}`}
                     element={<route.element />}
                   />
-                )),
+                ))
               )}
             </Route>
           </Routes>
+        </div>
+        <div className="on-this-page bg-red w-full">
+          <h3>Hello World!</h3>
         </div>
       </div>
     </>
